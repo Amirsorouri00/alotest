5841a0a3bc54cb323e9c68c9fcdaacfe017d1fb4
Alopeyk Test <uiyshedvrhdkdwaeoo@avxrja.com>
2020-09-05 02:00:29 -0700

    change user migration and token driver for pre-required configuration of token authentication.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email',191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('api_token', 80)
+                    ->after('password')
+                    ->unique()
+                    ->nullable()
+                    ->default(null);
             $table->rememberToken();
             $table->timestamps();
         });
